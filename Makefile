3b1cb649a231f0c6ccf79f579a66941df5a445ba
Nirmoy Das <ndas@suse.de>
2018-09-11 10:15:53 +0200

    skel 1.1

--- Makefile
+++ Makefile
@@ -11,20 +11,36 @@ SYNCIMAP_BUILD=`git rev-parse HEAD 2>/dev/null`
 SYNCIMAP_LDFLAGS = -ldflags "-X=main.Version=$(SYNCIMAP_VERSION) -X=main.Build=$(SYNCIMAP_VERSION)"
 SYNCIMAP_MAIN = cmd/syncimap/main.go
 all: build
+
+dep-exe:
+ifndef DEP_EXE
+	@echo ">>> dep does not seem to be installed. installing dep..."
+	go get -u github.com/golang/dep/cmd/dep
+endif
+
+dep-rebuild: dep-exe Gopkg.toml
+	@echo ">>> Rebuilding vendored deps (respecting Gopkg.toml constraints)"
+	rm -rf vendor Gopkg.lock
+	dep ensure -v && dep status
+
+dep-update: dep-exe Gopkg.toml
+	@echo ">>> Updating vendored deps (respecting Gopkg.toml constraints)"
+	dep ensure -update -v && dep status
+
+# download automatically the vendored deps when "vendor" doesn't exist
+vendor: dep-exe
+	@[ -d vendor ] || dep ensure -v
+
 build: 
 	$(GOBUILD)  $(SYNCIMAP_LDFLAGS) -o $(SYNCIMAP_BINARY) $(SYNCIMAP_MAIN)
 test: 
 	$(GOTEST) -v ./...
 clean: 
 	$(GOCLEAN)
-	rm -f $(BINARY_NAME)
-	rm -f $(BINARY_UNIX)
+	rm -f $(SYNCIMAP_BINARY)
 run:
-	$(GOBUILD) -o $(BINARY_NAME) -v ./...
-	./$(BINARY_NAME)
-deps:
-	$(GOGET) github.com/markbates/goth
-	$(GOGET) github.com/markbates/pop
+	$(GOBUILD)  $(SYNCIMAP_LDFLAGS) -o $(SYNCIMAP_BINARY) $(SYNCIMAP_MAIN)
+	./$(SYNCIMAP_BINARY)
 
 
 # Cross compilation
--- cmd/syncimap/main.go
+++ cmd/syncimap/main.go
@@ -2,8 +2,130 @@ package main
 
 import (
 	"log"
+	"crypto/tls"
+
+	"github.com/emersion/go-imap/client"
+	"github.com/emersion/go-imap"
+	"github.com/rthornton128/goncurses"
 )
 
+
+func print_email(menu_items []string) {
+	stdscr, err := goncurses.Init()
+	if err != nil {
+		log.Fatal("init:", err)
+	}
+	defer goncurses.End()
+
+	goncurses.Raw(true)
+	goncurses.Echo(false)
+	goncurses.Cursor(0)
+	stdscr.Clear()
+	stdscr.Keypad(true)
+
+	items := make([]*goncurses.MenuItem, len(menu_items))
+	for i, val := range menu_items {
+		items[i], _ = goncurses.NewItem("", val)
+		defer items[i].Free()
+	}
+
+	menu, err := goncurses.NewMenu(items)
+	if err != nil {
+		stdscr.Print(err)
+		return
+	}
+	defer menu.Free()
+
+	menu.Post()
+
+	stdscr.MovePrint(20, 0, "'q' to exit")
+	stdscr.Refresh()
+
+	for {
+		goncurses.Update()
+		ch := stdscr.GetChar()
+
+		switch goncurses.KeyString(ch) {
+		case "q":
+			return
+		case "down":
+			menu.Driver(goncurses.REQ_DOWN)
+		case "up":
+			menu.Driver(goncurses.REQ_UP)
+		}
+	}
+}
 func main() {
+	log.Println("Connecting to server...")
+	tlsConfig := &tls.Config{ServerName: "imap.suse.de", InsecureSkipVerify: true,}
+	// Connect to server
+	c, err := client.DialTLS("imap.suse.de:993", tlsConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected")
+
+	// Don't forget to logout
+	defer c.Logout()
+
+	log.Println("TLS started")
+
+	// Login
+	if err := c.Login("ndas", "@!l@B!@3"); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Logged in")
+
+	// List mailboxes
+	mailboxes := make(chan *imap.MailboxInfo, 10)
+	done := make(chan error, 1)
+	go func () {
+		done <- c.List("", "*", mailboxes)
+	}()
+
+	log.Println("Mailboxes:")
+	for m := range mailboxes {
+		log.Println("* " + m.Name)
+	}
+
+	if err := <-done; err != nil {
+		log.Fatal(err)
+	}
+
+	// Select INBOX
+	mbox, err := c.Select("INBOX/mailing_lists/openvpn", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Flags for INBOX:", mbox.Flags)
+
+	// Get the last 4 messages
+	from := uint32(2)
+	to := mbox.Messages
+	if mbox.Messages > 3 {
+		// We're using unsigned integers here, only substract if the result is > 0
+		from = mbox.Messages - 3
+	}
+	seqset := new(imap.SeqSet)
+	seqset.AddRange(from, to)
+
+	messages := make(chan *imap.Message, 2)
+	done = make(chan error, 1)
+	go func() {
+		done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
+	}()
+	log.Println("Last 4 messages:")
+	emails := []string{""}
+	for msg := range messages {
+		emails = append(emails, msg.Envelope.Subject)
+	}
+	elements := []string{"cat", "dog", "bird"}
+	log.Println(emails)
+	log.Println(elements)
+	print_email(emails)
+	if err := <-done; err != nil {
+		log.Fatal(err)
+	}
+
 	log.Println("Done!")
 }
